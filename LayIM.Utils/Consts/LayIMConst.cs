69639d7d17842705b85f84088ab013533c875641
Fanyuepan <fanpan26@126.com>
2016-08-30 13:27:53 +0800

    增加用户好友列表key

--- LayIM.Utils/Consts/LayIMConst.cs
+++ LayIM.Utils/Consts/LayIMConst.cs
@@ -19,5 +19,16 @@ namespace LayIM.Utils.Consts
         /// 保存用户登录token的hashkey
         /// </summary>
         public const string LayIM_Cache_UserLoginToken = "user_login_token";
+        /// <summary>
+        /// 某个聊天室用户key
+        /// 由于1v1聊天也是用group，发送消息时需要验证，两个人是否在同一聊天室，否则不能发送
+        /// 群组同理
+        /// </summary>
+        public const string LayIM_Group_OnLineUsers = "user_online_{0}";
+        /// <summary>
+        /// 所有在线用户key
+        /// </summary>
+        public const string LayIM_All_OnlineUsers = "user_online_all";
+        public const string LayIM_All_UserFriends = "friendlist:user_{0}";
     }
 }
