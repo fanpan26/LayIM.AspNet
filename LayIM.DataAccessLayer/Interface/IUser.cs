5370e3ea30b94565f246f12ca9754f675104ecc6
Fanyuepan <fanpan26@126.com>
2017-01-20 16:41:09 +0800

    用户接口

---
+++ LayIM.DataAccessLayer/Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using LayIM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayIM.DataAccessLayer.Interface
+{
+    public interface IUser
+    {
+        BaseListResult GetUserBase(int userId);
+    }
+}
