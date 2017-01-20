0e3beb926c44c2698780060feeb060a7826f4145
Fanyuepan <fanpan26@126.com>
2017-01-20 16:41:17 +0800

    数据层逻辑注册

---
+++ LayIM.DataAccessLayer/DataAccessLayerService.cs
@@ -0,0 +1,25 @@
+﻿using LayIM.Container;
+using LayIM.DataAccessLayer.Classes;
+using LayIM.DataAccessLayer.Interface;
+using LayIM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayIM.DataAccessLayer
+{
+    public class DataAccessLayerService
+    {
+        /// <summary>
+        /// 服务需要在BLL中注册
+        /// </summary>
+        public static void RegisterService()
+        {
+            LayIMDataAccessLayerContainer.GlobalContainer.Register<IUser, User>()
+                                                         .Register<IMultipleHandler<BaseListResult>, UserBaseListHandler>();
+                
+        }
+    }
+}
