3104c1c97b0740c768dde0712fbcab8a32277744
Fanyuepan <fanpan26@126.com>
2017-01-20 16:41:03 +0800

    用户基础逻辑

---
+++ LayIM.DataAccessLayer/Classes/User.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using LayIM.DataAccessLayer.Helper;
+using LayIM.DataAccessLayer.Interface;
+using LayIM.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayIM.DataAccessLayer.Classes
+{
+    public class User : IUser
+    {
+        private IMultipleHandler<BaseListResult> _handler;
+        public User(IMultipleHandler<BaseListResult> handler)
+        {
+            _handler = handler;
+        }
+        /// <summary>
+        /// 获取基本用户数据
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public BaseListResult GetUserBase(int userId)
+        {
+            const string spName = "[Proc_LayIM_GetUserInitInfo_RC]";
+            DynamicParameters parameter = new DynamicParameters();
+            parameter.Add("@userid", userId, DbType.Int32);
+            if (_handler == null)
+            {
+                throw new ArgumentOutOfRangeException("IMultipleHandler<BaseListResult> 服务未注册");
+            }
+            return DBHelper.QueryMultiple(spName, parameter, CommandType.StoredProcedure, _handler);
+        }
+    }
+}
