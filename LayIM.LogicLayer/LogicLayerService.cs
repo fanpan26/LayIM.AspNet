ad27b2968a39da1dd6da957eba2eecb9a3d8cc64
Fanyuepan <fanpan26@126.com>
2017-01-20 16:41:29 +0800

    增加逻辑层

---
+++ LayIM.LogicLayer/LogicLayerService.cs
@@ -0,0 +1,26 @@
+﻿using LayIM.Container;
+using LayIM.DataAccessLayer;
+using LayIM.DataAccessLayer.Interface;
+using LayIM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayIM.LogicLayer
+{
+    /// <summary>
+    /// 注册公共服务
+    /// </summary>
+    public class LogicLayerService
+    {
+        /// <summary>
+        /// 注册服务
+        /// </summary>
+        public static void RegisterDataService()
+        {
+            DataAccessLayerService.RegisterService();
+        }
+    }
+}
